aa94a3a4d7929573936bc5cba0b44dc7b5c1a043
Fernando Zamora <fernandozamorarico@gmail.com>
2023-10-06 12:21:36 +0200

    Ejercicio 2

---
+++ AsincroniaEnNode/Ejercicio2.js
@@ -0,0 +1,16 @@
+// En este ejercicio, exploraremos la asincronía en Node.js y cómo afecta a la pila de llamadas.
+
+// Crea una función llamada imprimirMensaje que tome un mensaje como argumento y lo imprima en la consola.
+
+// Llama a imprimirMensaje("Hola, Mundo") y observa el resultado.
+
+// Ahora, utiliza setTimeout para hacer que imprimirMensaje se llame después de 2 segundos con el mismo mensaje. ¿Cómo cambia la ejecución?
+
+// Escribe el flujo de ejecución de las llamadas y observa cómo las funciones se agregan y eliminan de la pila de llamadas.
+
+function imprimirMensaje(mensaje){
+    return mensaje;
+}
+
+console.log(imprimirMensaje("Hola, Mundo"));
+
